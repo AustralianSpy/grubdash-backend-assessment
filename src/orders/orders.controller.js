3836eb56bb8d8867d76c484aafed1833dd37cb85
Sydney Workman <sworkman1996@gmail.com>
2022-04-11 21:58:04 -0500

    delete route, comments

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -101,6 +101,7 @@ function doesIdMatch(req, res, next) {
 }
 
 // middleware to check if order has an appropriate 'status' property.
+// reject if invalid status, empty status, or already delivered.
 function statusIsValid(req, res, next) {
     const { data = {} } = req.body;
     const status = data['status'];
@@ -113,16 +114,37 @@ function statusIsValid(req, res, next) {
     next({ status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered` });
 }
 
+// route to update a pre-existing order.
 function update(req, res) {
     const order = res.locals.order;
     const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
     
+    // new properties.
     order.deliverTo = deliverTo;
     order.mobileNumber = mobileNumber;
     order.status = status;
     order.dishes = dishes;
 
     res.json({ data: order });
+}
+
+// middleware to make sure an order requested for deletion isn't pending.
+function isOrderPending(req, res, next) {
+    const order = res.locals.order;
+    if (order.status !== 'pending') {
+        return next({ status: 400, message: `An order cannot be deleted unless it is pending` });
+    }
+    next();
+}
+
+// delete a non-pending order.
+function destroy(req, res) {
+    const order = res.locals.order;
+    const { orderId } = req.params;
+    const index = orders.findIndex((order) => order.id == orderId);
+
+    const deletedOrders = orders.splice(index, 1);
+    res.sendStatus(204);
 }
 
 module.exports = {
@@ -152,6 +174,11 @@ module.exports = {
         quantityIsValid,
         statusIsValid,
         update,
+    ],
+    delete: [
+        orderExists,
+        isOrderPending,
+        destroy,
     ],
     list,
 }
